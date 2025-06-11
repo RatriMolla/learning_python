c980f2cae5d2acc38a635946825051167a38e8e0
RatriMolla <mafrujaratri@gmail.com>
2025-06-11 21:12:23 +0600

    variable

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # learning_python
 phyton learning code
-here code
-vj fvn
-mv gv
---
+++ phython.py
@@ -0,0 +1,15 @@
+#output
+print("hello world")
+print("it is my first code in python.","i will try my best")
+print(23)
+print(23+5)
+name="ratri"#string
+age=22
+print(name)#variable print
+print ("my name is :",name)
+print("my age is :", age)
+age2=age
+
+print("my age is :", age2)
+
+
