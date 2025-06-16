d9ca80c25d38332b273009632e72a0658b5a6311
RatriMolla <mafrujaratri@gmail.com>
2025-06-16 18:33:51 +0600

    pbs 2

--- Practice_problem.py
+++ Practice_problem.py
@@ -1,3 +1,34 @@
 b=5.78
 a=int(b)
 print(a)
+#1write  a program to input 2 number and print sum
+
+a=int (input("a:") )
+
+b= int (input("b:") )
+sum=a+b
+print("sum:",sum)
+
+#2wap to side of square and print it's area
+
+side=int(input("side="))
+
+area=side*side
+print("area=",area)
+
+
+#3wap to input 2 floating point number & print their average
+
+a=float (input("a:") )
+
+b=float (input("b:") )
+
+average=(a+b)/2
+print("averag=", average)
+
+
+#4wap to inpurt 2 int number a,b print true if a is greater than or equal to b .if not print false
+a=int (input("a:") )
+
+b= int (input("b:") )
+print(a>=b)
