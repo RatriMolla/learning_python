9d362bfc23449a24750fa0a246fe179c91f650e7
RatriMolla <mafrujaratri@gmail.com>
2025-06-28 22:09:18 +0600

    pbs8

--- problem.py
+++ problem.py
@@ -11,50 +11,59 @@
 # def add(a,b):
 #     return a+b
 
-marks =[89.4, 88, 89, 81, 90]  #in list we can stores different types of data and we can change value
-print(marks)
-print (marks[4])
-print(len(marks))
-student =["ratri", "cse",25]
-print(student)
-student[0]="mafruja" 
-print(student)
-print(marks[1:5])
-print(marks[-5:-1])
+# marks =[89.4, 88, 89, 81, 90]  #in list we can stores different types of data and we can change value
+# print(marks)
+# print (marks[4])
+# print(len(marks))
+# student =["ratri", "cse",25]
+# print(student)
+# student[0]="mafruja" 
+# print(student)
+# print(marks[1:5])
+# print(marks[-5:-1])
 
-#method of list
-list=[2,1,4]
-list.append(5)# add element in the last
+# #method of list
+# list=[2,1,4]
+# list.append(5)# add element in the last
 
-list.sort()#sort ascending order
+# list.sort()#sort ascending order
+# print(list)
+# list.sort(reverse=True)#decending order
+# print(list)
+# list1=["rose","lily","nightqueen","lotus","sunflower"]
+# list1.sort()
+# print(list1)
+# list1.reverse()# write the list from the end:reverse
+# print(list1)
+
+# list1.insert(3,"jesmin")#inser element in particuler element .insert(index,element)
+# print(list1)
+
+# list1.remove("jesmin")#remove first occurrence of  element
+# print(list1)
+
+# list1.pop(3)#remove element according to index number
+# print(list1)
+
+# #Tuples
+
+# tup=(1,)#single element tuple,without comma it's just a integer number
+# print(tup)
+# print(type(tup))
+
+# tpl=(3,5,6,8,3,6,)
+# print(tpl[1:3])
+
+# tpl.index(5)
+# print(tpl.index(5))#.index(element), it find index of value
+
+# print(tpl.count(6))# count how may time element has in tuple
+list=[]
+mov1=input("enter 1st movie:")
+mov2=input("enter 2st movie:")
+mov3=input("enter 3st movie:")
+
+list.append(mov1)
+list.append(mov2)
+list.append(mov3)
 print(list)
-list.sort(reverse=True)#decending order
-print(list)
-list1=["rose","lily","nightqueen","lotus","sunflower"]
-list1.sort()
-print(list1)
-list1.reverse()# write the list from the end:reverse
-print(list1)
-
-list1.insert(3,"jesmin")#inser element in particuler element .insert(index,element)
-print(list1)
-
-list1.remove("jesmin")#remove first occurrence of  element
-print(list1)
-
-list1.pop(3)#remove element according to index number
-print(list1)
-
-#Tuples
-
-tup=(1,)#single element tuple,without comma it's just a integer number
-print(tup)
-print(type(tup))
-
-tpl=(3,5,6,8,3,6,)
-print(tpl[1:3])
-
-tpl.index(5)
-print(tpl.index(5))#.index(element), it find index of value
-
-print(tpl.count(6))# count how may time element has in tuple
