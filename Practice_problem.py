2835cca3222f72d3078a026b4193d6dba12c1704
RatriMolla <mafrujaratri@gmail.com>
2025-07-05 22:13:18 +0600

    pbs10,11,14

--- Practice_problem.py
+++ Practice_problem.py
@@ -144,6 +144,42 @@ table={
 
 print(table)
 
+#print number from 1 to 100
+
+i=1
+while i <=100:
+    print(i)
+    i+=1
+# print 100 to 1    
+i=100
+while i >=1:
+    print(i)
+    i-=1
+#print the multipication table of number n    
+
+n=int(input("n="))  
+a=1
+while a<=10:
+    print(n*a)
+    a+=1
+
+n=int(input("n="))  
+a=1
+while a<=50:
+    print(a)
+    a+=5
+#   print the element of the following list using loop
+list =[1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+idx=0
+while idx<len(list):
+    # print(idx)
+    print(list[idx])
+    idx+=1
+
+
+
+
 
 
     
--- fhjf.py
+++ fhjf.py
@@ -20,9 +20,43 @@
 #     count += 1
 # print(count)
 
-i=5
-while i>=1:
+# i=5
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# print(i)
+#print number from 1 to 100
+
+i=1
+while i <=100:
+    print(i)
+    i+=1
+# print 100 to 1    
+i=100
+while i >=1:
     print(i)
     i-=1
+#print the multipication table of number n    
 
-print(i)
+n=int(input("n="))  
+a=1
+while a<=10:
+    print(n*a)
+    a+=1
+
+n=int(input("n="))  
+a=1
+while a<=50:
+    print(a)
+    a+=5
+#   print the element of the following list using loop
+list =[1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+idx=0
+while idx<len(list):
+    # print(idx)
+    print(list[idx])
+    idx+=1
+
+
